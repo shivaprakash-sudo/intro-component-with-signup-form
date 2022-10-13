61161ca58c6017f5fd2ff49dfe04db5e361fdff6
Shiva Prakash Pendem <pendemsivaprakash@gmail.com>
2022-10-14 00:56:13 +0100

    add mobile styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,13 @@ module.exports = {
       fontFamily: {
         sans: ["Poppins", ...defaultTheme.fontFamily.sans],
       },
+      backgroundImage: {
+        mobile: "url('../images/bg-intro-mobile.png')",
+        desktop: "url('../images/bg-intro-desktop.png')",
+      },
+      boxShadow: {
+        theme: "0 0.5rem 0.1rem rgba(0, 0, 0, 0.2)",
+      },
     },
   },
   plugins: [],
